8f9bcba2679fc2f70471a1c185423759de394339
Shanthi <shanthi.arunachaleam.dev@gmail.com>
2026-04-21 14:29:40 +0530

    login show  modified

--- src/Component/SocialMedia/Login.jsx
+++ src/Component/SocialMedia/Login.jsx
@@ -8,7 +8,7 @@ function Login({ setIsLogIn, isLoginPage = true }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const [showPassword, setShowPassword] = useState("hide");
+  const [showPassword, setShowPassword] = useState("show");
 
   useEffect(() => {
     const token = localStorage.getItem("token");
